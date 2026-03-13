77c88d8052934cee94a84d80be5c049fb8763af3
Martzz-a <maritzafidelyah@gmail.com>
2026-03-13 14:34:01 +0700

    Menambahkan Prosedur Insertion Sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -27,3 +27,27 @@ void input(){
         cin >> arr[i];
     }
 }
+
+void insertionSort(){
+    //procesure insertion sort
+
+    int temp;
+    int j;
+
+    for(int i = 0; i < n; i++){
+        temp = arr[i];
+        j = i - 1;
+
+        while(j >= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j = 1] = temp;
+        cout << "\nStep" << i << ": ";
+
+        for(int k = 0; k< n; k++){
+            cout << arr[k] << " ";
+        }
+    }
+}
